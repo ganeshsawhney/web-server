338824a47ad2d11fb79d356220f893c72a81c157
Ganesh Sawhney <ganeshsawhney@gmail.com>
2016-04-02 01:49:14 +0530

    Refractoring and Making new file middleware.js

--- server.js
+++ server.js
@@ -2,18 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		//route-level middleware
-		//next used as authentication should run 1st ,next used to move forward only when needed
-		console.log('private route hit!');
-		next();
-	},
-	logger: function (req, res, next) {
-		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
